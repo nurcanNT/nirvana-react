24249d5232d0d69c7b59b9a53199147c20297425
nurcanNT <nrcntpkra@gmail.com>
2023-03-02 01:00:33 +0300

    Updates

---
+++ static/media/index.f9ac19be921da9f14331.js
@@ -0,0 +1,1 @@
+import React from"react";import ReactDOM from"react-dom/client";import"./index.css";import App from"./App";import reportWebVitals from"./reportWebVitals";import{jsx as _jsx}from"react/jsx-runtime";var root=ReactDOM.createRoot(document.getElementById("root"));root.render(_jsx(React.StrictMode,{children:_jsx(App,{})})),reportWebVitals();
